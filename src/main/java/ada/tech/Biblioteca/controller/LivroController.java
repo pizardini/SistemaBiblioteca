0c2e7b87c80a74e8df17748de0284895992e9b06
Pietro Zardini <pizardini@gmail.com>
2023-04-01 19:20:54 -0300

    adição de try catch para tratamento de erro no controller de livro

--- src/main/java/ada/tech/Biblioteca/controller/LivroController.java
+++ src/main/java/ada/tech/Biblioteca/controller/LivroController.java
@@ -3,6 +3,7 @@ package ada.tech.Biblioteca.controller;
 import ada.tech.Biblioteca.model.dto.LivroDTO;
 import ada.tech.Biblioteca.service.EditoraService;
 import ada.tech.Biblioteca.service.LivroService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,35 +13,62 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
+@Slf4j
 public class LivroController {
 
     @Autowired
     private LivroService livroService;
 
     @GetMapping
-    public ResponseEntity<List<LivroDTO>> listar() {
-        return ResponseEntity.ok(livroService.listar());
+    public ResponseEntity<Object> listar() {
+        try {
+            return ResponseEntity.ok(livroService.listar());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
     }
 
     @GetMapping("/id")
-    public ResponseEntity<LivroDTO> pegarUm(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(livroService.pegarPorId(id));
+    public ResponseEntity<Object> pegarUm(@PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(livroService.pegarPorId(id));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
     }
 
     @PostMapping
-    public ResponseEntity<LivroDTO> criar(@RequestBody LivroDTO livroDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(livroService.criar(livroDTO));
+    public ResponseEntity<Object> criar(@RequestBody LivroDTO livroDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(livroService.criar(livroDTO));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<LivroDTO> editar(@RequestBody LivroDTO livroDTO, @PathVariable("id") Long id) {
-        return ResponseEntity.ok(livroService.editar(livroDTO, id));
+    public ResponseEntity<Object> editar(@RequestBody LivroDTO livroDTO, @PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(livroService.editar(livroDTO, id));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletar(@PathVariable("id") Long id) {
-        livroService.deletar(id);
-
-        return ResponseEntity.ok("Livro com id "+id+" removido com sucesso");
+        try {
+            livroService.deletar(id);
+            return ResponseEntity.ok("Livro com id "+id+" removido com sucesso");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
